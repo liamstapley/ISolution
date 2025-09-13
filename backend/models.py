5387a94d7f9036d0c44dccf25173a245574240b0
liamstapley <stapley.liam@gmail.com>
2025-09-13 04:49:56 -0400

    edited the relative paths in the imports

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from database import Base
+from .database import Base
 
 # --- Many-to-many: users attending events ---
 event_attendees = Table(
